c69cafb2e202b924ca7aa0aa23a3d3f4b75baa59
Nithish <85347782+nithish576s@users.noreply.github.com>
2023-02-08 22:21:37 +0530

    Add files via upload

--- quiz7/11.10.2.8/codes/11_10_2_8.py
+++ quiz7/11.10.2.8/codes/11_10_2_8.py
@@ -6,30 +6,39 @@ sys.path.insert(0,'/home/nithish/Downloads/training-main/math/codes/CoordGeo/')
 from line.funcs import *
 
 
-n = np.array([np.cos(np.pi/6), np.sin(np.pi/6)])
+n1 = np.array([np.cos(np.pi/6), np.sin(np.pi/6)])
+n2 = np.array([np.cos(-np.pi/6), np.sin(-np.pi/6)])
 
 orig = np.array([0,0])
 
 d = 5
 
-c = np.abs(n.T@np.zeros_like(n) - d)/np.linalg.norm(n)
+c1 = np.abs(n1.T@np.zeros_like(n1) - d)/np.linalg.norm(n1)
+c2 = np.abs(n2.T@np.zeros_like(n2) - d)/np.linalg.norm(n2)
 
-print('normal vector of line:\n',n)
-print('c: ',c)
+print('normal vector of line 1:\n',n1)
+print('c: ',c1)
+print('normal vector of line 2:\n',n2)
+print('c: ',c2)
 
-dir_vec =  norm_vec(orig,n)
+dir_vec1 =  norm_vec(orig,n1)
+dir_vec2 =  norm_vec(orig,n2)
 
-line = line_dir_pt(dir_vec,d*n,-3,3)
-normal = line_gen(np.array([0,0]),d*n)
-plt.scatter(d*n[0],d*n[1])
+line1 = line_dir_pt(dir_vec1,d*n1,-5,5)
+normal1 = line_gen(np.array([0,0]),d*n1)
+plt.scatter(d*n1[0],d*n1[1])
 
-plt.annotate('Feet of perpendicular',(d*n[0],d*n[1]))
+line2 = line_dir_pt(dir_vec2,d*n2,-5,5)
+normal2 = line_gen(np.array([0,0]),d*n2)
+plt.scatter(d*n2[0],d*n2[1])
 
 
-plt.plot(line[0,:],line[1,:],label="line")
-plt.plot(normal[0,:],normal[1,:],label="normal")
-plt.legend(['line','normal'])
+plt.plot(line1[0,:],line1[1,:],label="line")
+plt.plot(normal1[0,:],normal1[1,:],label="normal")
+plt.plot(line2[0,:],line2[1,:],label="line")
+plt.plot(normal2[0,:],normal2[1,:],label="normal")
 
+plt.legend(['line 1','normal 1','line 2','normal 2'])
 
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
