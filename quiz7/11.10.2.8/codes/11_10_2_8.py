ddbbfe094b9272d30026bfedfd3211fee3d1afdf
Nithish <85347782+nithish576s@users.noreply.github.com>
2023-02-09 13:19:48 +0530

    Add files via upload

--- quiz7/11.10.2.8/codes/11_10_2_8.py
+++ quiz7/11.10.2.8/codes/11_10_2_8.py
@@ -7,30 +7,29 @@ from line.funcs import *
 
 
 n1 = np.array([np.cos(np.pi/6), np.sin(np.pi/6)])
-n2 = np.array([np.cos(-np.pi/6), np.sin(-np.pi/6)])
 
 orig = np.array([0,0])
 
 d = 5
 
 c1 = np.abs(n1.T@np.zeros_like(n1) - d)/np.linalg.norm(n1)
-c2 = np.abs(n2.T@np.zeros_like(n2) - d)/np.linalg.norm(n2)
+c2 = -np.abs(n1.T@np.zeros_like(n1) - d)/np.linalg.norm(n1)
 
 print('normal vector of line 1:\n',n1)
 print('c: ',c1)
-print('normal vector of line 2:\n',n2)
+print('normal vector of line 2:\n',n1)
 print('c: ',c2)
 
 dir_vec1 =  norm_vec(orig,n1)
-dir_vec2 =  norm_vec(orig,n2)
+dir_vec2 =  norm_vec(orig,n1)
 
 line1 = line_dir_pt(dir_vec1,d*n1,-5,5)
 normal1 = line_gen(np.array([0,0]),d*n1)
 plt.scatter(d*n1[0],d*n1[1])
 
-line2 = line_dir_pt(dir_vec2,d*n2,-5,5)
-normal2 = line_gen(np.array([0,0]),d*n2)
-plt.scatter(d*n2[0],d*n2[1])
+line2 = line_dir_pt(dir_vec2,-1*d*n1,-5,5)
+normal2 = line_gen(np.array([0,0]),-1*d*n1)
+plt.scatter(-1*d*n1[0],-1*d*n1[1])
 
 
 plt.plot(line1[0,:],line1[1,:],label="line")
